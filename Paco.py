6d3d55db9abc13e703095c88a5abd9a920eb04e0
Paco-Murillo <jfmurillol@gmail.com>
2020-10-04 13:13:58 -0500

    Final

--- Paco.py
+++ Paco.py
@@ -1,3 +1,14 @@
+from itertools import combinations
+
+
+def createDict(finishedTransitionArray, statesArray, alphabetArray):
+    for index in range(len(statesArray)):
+        finishedTransitionArray.append([])
+        list = finishedTransitionArray[index]
+        for index in range(len(alphabetArray)):
+            list.append([])
+
+
 def leerArchivo(dirTxtFile):
     file = open(dirTxtFile, 'r', encoding='UTF-8')
     transitionArray = file.read().split('),(')
@@ -22,10 +33,82 @@ def leerArchivo(dirTxtFile):
                 alphabetAndStateFlag = False
             else:
                 workingArray.append(transitionArray[index][indexIn])
-    print(transitionArray)
+
+    finishedTransitionArray = []
+    createDict(finishedTransitionArray, statesArray, alphabetArray)
+
+    for list in transitionArray:
+        stateIndex = statesArray.index(list[1])
+        letterIndex = alphabetArray.index(list[0])
+        finishedTransitionArray[stateIndex][letterIndex].append(list[2])
+    
+    return finishedTransitionArray, alphabetArray, statesArray
+
+
+def createPowersetStates(statesArray):
+    DFAStates = []
+    for index in range(len(statesArray)+1):
+        for comb in combinations(statesArray, index):
+            stateName = ""
+            for string in comb:
+                stateName += string
+            DFAStates.append(stateName)
+
+    return DFAStates
+
+
+def createTransitions(DFAStates, statesArray, alphabetArray, transitionArray):
+    DFATransitions = []
+    createDict(DFATransitions, DFAStates, alphabetArray)
+    for indexAlpha in range(len(alphabetArray)):
+        for indexStates in range(len(DFAStates)):
+            workingList = DFATransitions[indexStates][indexAlpha]
+            for letterState in DFAStates[indexStates]:
+                for outState in transitionArray[statesArray.index(letterState)][indexAlpha]:
+                    try:
+                        if workingList[0].find(outState) == -1:
+                            workingList[0] += outState
+                    except IndexError:
+                        workingList.append(outState)
+
+    return DFATransitions
+
+
+def createTxt(DFAStates, DFATransitions, alphabetArray):
+    outString = "{"
+
+    for indexState in range(len(DFAStates)):
+        for indexAlpha in range(len(alphabetArray)):
+            try:
+                outString += "("+alphabetArray[indexAlpha]+","+DFAStates[indexState]+","+DFATransitions[indexState][indexAlpha][0]+"),"
+            except IndexError:
+                if indexState != 0:
+                    outString += "(" + alphabetArray[indexAlpha] + "," + DFAStates[indexState] + ",∅),"
+                else:
+                    outString += "(∅,∅,∅),"
+                    break
+    outString = outString[:len(outString)-1:]+"}"
+
+    file = open("DFA.txt", 'w', encoding='UTF-8')
+    file.write(outString)
+    file.close()
+
+
+
+def main():
+    transitionArray, alphabetArray, statesArray = leerArchivo("prueba.txt")
     print(alphabetArray)
     print(statesArray)
-    
-    return transitionArray
+    print(transitionArray)
+    DFAStates = createPowersetStates(statesArray)
+    print(DFAStates)
+    DFATransitions = createTransitions(DFAStates, statesArray, alphabetArray, transitionArray)
+    print(DFATransitions)
+    createTxt(DFAStates, DFATransitions, alphabetArray)
 
-leerArchivo("prueba.txt")
+
+    # {(1,p,p),(0,p,q),(0,q,r),(1,q,r),(0,r,s),(1,r,p),(0,s,s),(1,s,s)}
+    # 1*0E0E*
+
+
+main()
